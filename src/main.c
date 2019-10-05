cfb267711773676f041e89174cff1418a4f5948a
y21 <artur.fiedler54@gmail.com>
2019-10-05 13:47:04 +0200

    add more questions and validate_snowflake

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 // Constants
 #define VERSION "1.2"
 #define MAX_TOKEN_LEN 64
 #define MAX_PREFIX_LEN 8
+#define MAX_PRESENCE_TYPE_LEN 16
 #define MAX_PRESENCE_NAME_LEN 32
 #define MAX_SNOWFLAKE_LEN 20
 #define MAX_IGNORED_SERVERS MAX_PREFIX_LEN
@@ -11,10 +13,11 @@
 #define MAX_EXECUTORS MAX_PREFIX_LEN
 #define MAX_PERMISSIONS MAX_PREFIX_LEN
 #define MAX_PERMISSION_LEN MAX_COMMAND_NAME_LEN
+#define MAX_GUILD_LEN 16
 
 // Structs
 typedef struct {
-	int type;
+	char type[MAX_PRESENCE_TYPE_LEN];
 	char name[MAX_PRESENCE_NAME_LEN];
 } presence;
 
@@ -22,6 +25,7 @@ typedef struct {
 	char id[MAX_SNOWFLAKE_LEN];
 	char verification_channel[MAX_SNOWFLAKE_LEN];
 	char verification_role[MAX_SNOWFLAKE_LEN];
+	int valid;
 } guild;
 
 typedef struct {
@@ -35,7 +39,19 @@ char token[MAX_TOKEN_LEN];
 char prefix[MAX_PREFIX_LEN];
 int delete_messages;
 presence prs;
+guild servers[MAX_GUILD_LEN];
+int guild_count;
 int ignored_servers[MAX_IGNORED_SERVERS];
+
+
+int validate_snowflake(char snf[MAX_SNOWFLAKE_LEN]) {
+	for(int i = 0; i < strlen(snf); ++i) {
+		if (snf[i] < 48 || snf[i] > 57) {
+			return 0;
+		}
+	}
+	return strlen(snf) > 15;
+}
 
 // Main
 int main() {
@@ -62,6 +78,51 @@ int main() {
 			printf("Delete messages (1 = yes, 0 = no): ");
 			scanf("%d", &delete_messages);
 			getchar(); // \n
+			if (delete_messages > 1 || delete_messages < 0) {
+				fprintf(stderr, "[x] delete_messages must have a value between 0 and 1.");
+				exit(1);
+			}
+
+			printf("Presence type (leave blank if no presence): ");
+			fgets(prs.type, sizeof(prs.type), stdin);
+
+			if (!strcmp(prs.name, "")) {
+				printf("Presence name: ");
+				fgets(prs.name, sizeof(prs.name), stdin);
+			}
+
+			printf("To how many servers should discordcaptcha listen to (1-16): ");
+			scanf("%d", &guild_count);
+			getchar();
+
+			if (guild_count > 16 || guild_count < 1) {
+				fprintf("[x] guild_count must have a value between 1 and 16.");
+				exit(1);
+			}
+
+			for(int i = 0; i < guild_count; ++i) {
+				printf("[G%d] Guild ID: ", i + 1);
+				fgets(servers[i].id, sizeof(servers[i].id), stdin);
+
+				printf("[G%d] Verification Channel ID: ", i + 1);
+				fgets(servers[i].verification_channel, sizeof(servers[i].verification_channel), stdin);
+
+				printf("[G%d] Verification Role ID: ", i + 1);
+				fgets(servers[i].verification_role, sizeof(servers[i].verification_role), stdin);
+
+				if (!validate_snowflake(servers[i].id)) {
+					printf("[x] [G%d] Invalid Guild ID\n", i + 1);
+					servers[i].valid = 0;
+				} else if (!validate_snowflake(servers[i].verification_channel)) {
+					printf("[x] [G%d] Invalid Verification Channel ID\n", i + 1);
+					servers[i].valid = 0;
+				} else if (!validate_snowflake(servers[i].verification_role)) {
+					printf("[x] [G%d] Invalid Verification Role ID\n", i + 1);
+					servers[i].valid = 0;
+				} else {
+					servers[i].valid = 1;
+				}
+			}
 		}
 		break;
 		case 2:
